97183289c83e5a948690732247d5675c6cd11023
Oscar Urizar <oscar.m.Urizar.jr@gmail.com>
2021-09-24 23:49:29 -0400

    Added divs, text areas and buttons linked them to styles with id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // HW is covered on 09/23/2021 3 minutes into the video. Mini Project for unit 5 will help tremendously on the HW. The HW is talked about for 7 minutes
 // moment.js is discussed on 9/22/2021 1 hr 46 min 30 sec in.
+//Boot strap is discussed in 09/21/2021 02 hr 07 minutes
 //  Psuedo code
 // I need to create a daily planner to create a schedule. When the user opens the planner the should :
 // 1. They should see the current day is displayed at the top of the calendar.
