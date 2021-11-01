3793db8f032a1930367650de46c25fb4270aa0a2
Oscar Urizar <oscar.m.Urizar.jr@gmail.com>
2021-11-01 15:02:26 -0400

    Completed project, updated the README file,  and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
+//Letting DOM know I will be using jQuery
 $(document).ready(function () {
+
+    // Save button function listening for clicks
   $(".saveBtn").on("click", function () {
+
+    //getting time and text values
     var value = $(this).siblings(".text-description").val();
     var time = $(this).parent().attr("id");
+
+    //saving to local storage
     localStorage.setItem(time, value);
+
+    //show notification that items were saved
+    $('.notification').addClass('show');
+
+    // remove 'show' class after 5 seconds
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 5000);
+
   });
 
   function updateHour() {
     var currentHr = moment().hours();
+    //loop over time blocks
     $(".time-block").each(function () {
-      var blockHr = mom;
-
+      var blockHr = parseInt($(this).attr('id').split('-')[1]);
+        //check to see if we are in current time
       if (blockHr < currentHr) {
         $(this).addClass("past");
       } else if (blockHr === currentHr) {
@@ -25,9 +42,10 @@ $(document).ready(function () {
  
   updateHour();
 
+  //Check every 15 seconds to see if time is updated
   var interval = setInterval(updateHour, 15000);
 
-
+  //load data saved into local storage
   $("#hour-9 .text-description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .text-description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .text-description").val(localStorage.getItem("hour-11"));
@@ -38,5 +56,6 @@ $(document).ready(function () {
   $("#hour-16 .text-description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .text-description").val(localStorage.getItem("hour-17"));
 
+//   Show the current day on the page.
   $("#current-day").text(moment().format("dddd, MMMM Do"));
 });
