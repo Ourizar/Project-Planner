1a5a3296dea75183e2816699a5942bc16beb07b2
Oscar Urizar <oscar.m.Urizar.jr@gmail.com>
2021-10-31 20:06:11 -0400

    updated and completed project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,42 @@
-// HW is covered on 09/23/2021 3 minutes into the video. Mini Project for unit 5 will help tremendously on the HW. The HW is talked about for 7 minutes
-// moment.js is discussed on 9/22/2021 1 hr 46 min 30 sec in.
-//Boot strap is discussed in 09/21/2021 02 hr 07 minutes
-//  Psuedo code
-// I need to create a daily planner to create a schedule. When the user opens the planner the should :
-// 1. They should see the current day is displayed at the top of the calendar.
-// 2. When the user scrolls down they should see time blocks for standard business hours so from 8 - 5 pm.
-// 3. When they view the timeblocks for that day- each timeblock is color coded to indicate whether its in the past present or future.
-// 4. When they click each timeblock they can enter an event.
-// 5. When they clock the save button for that timeblock then the test for that event is stored into local storage.
-// 6. When the page is refreshed then the saved event persists.
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".text-description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
+  });
 
-// var timeDisplayEl = $("currentDay");
+  function updateHour() {
+    var currentHr = moment().hours();
+    $(".time-block").each(function () {
+      var blockHr = mom;
 
-// function displayToday(){
-    // timeDisplayEl = moment().format("MMM Do, YYYY")
-    // console.log(todaysDate);
-    
-var today = moment();
+      if (blockHr < currentHr) {
+        $(this).addClass("past");
+      } else if (blockHr === currentHr) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+ 
+  updateHour();
 
-$("#currentDay").text(today.format("MMM Do, YYYY"));
-console.log(today)
-
-
-
-
-
-
-
-
+  var interval = setInterval(updateHour, 15000);
 
 
+  $("#hour-9 .text-description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .text-description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .text-description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .text-description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .text-description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .text-description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .text-description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .text-description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .text-description").val(localStorage.getItem("hour-17"));
 
-
-
-
-
-
-
-
-
-
-
-
+  $("#current-day").text(moment().format("dddd, MMMM Do"));
+});
